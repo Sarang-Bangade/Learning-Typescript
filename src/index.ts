8c0e3d191f9dba5ec651e03e42823e7597269a50
Sarang-Bangade <bangadesarang@gmail.com>
2025-12-08 15:25:47 +0530

    comments added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+
+// It is simple as javascript but only the changes lies is..
+
+/* define the type of every variable which needs to be assigned and also
+   the return type of a function "(name: string)" instead of just "name" in the parameter
+    and also (name:string) : "string" the type that the function will return
+    the return statements is only return in backticks 
+*/
 function greet ( name: string): string {
     return `hello ${name}, welcome`;
 }
 
+
+// at declaration of the variable we also assign the type of variable 
 const username: string = "Sarang TypeScript"
 console.log(greet(username));

819695d31357d9cadf3d899973b152369058758f
Sarang-Bangade <bangadesarang@gmail.com>
2025-12-08 15:38:27 +0530

    New comments added in typesInTs file

---
+++ src/typeinTS.ts
@@ -0,0 +1,21 @@
+
+/* 
+    In typescript we dont have to tell the every datatype to assign
+    in variable some of them it takes by itself which is known as "inference"
+
+    and
+
+    in "Annotation" we will tell means we will assign the type manually
+*/
+
+let drink = "tea";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "SarangGod"
+
+let name:string = "SarangBhai"
+name = "Sayali"
+
+
+

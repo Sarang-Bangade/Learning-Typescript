360896b128b37158b73173557445d34aab1ca2b9
Sarang-Bangade <bangadesarang@gmail.com>
2025-12-08 13:49:33 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello ${name}, welcome`;
+}
+const username = "Sarang TypeScript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function greet ( name: string): string {
+
+    return `hello ${name}, welcome`;
+
+}
+
+const username: string = "Sarang TypeScript"
+console.log(greet(username));
